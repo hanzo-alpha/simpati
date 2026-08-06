9779de708575d6623ece540474dd722ee1a28442
Hanzo Alpha <hansen.makangiras@hotmail.com>
2026-08-06 15:05:31 +0800

    Fix composer test, add phpstan.neon, and clean FortifyServiceProvider actions

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Actions\Fortify\CreateNewUser;
-use App\Actions\Fortify\ResetUserPassword;
 use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
@@ -41,8 +39,7 @@ class FortifyServiceProvider extends ServiceProvider
      */
     private function configureActions(): void
     {
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-        Fortify::createUsersUsing(CreateNewUser::class);
+        // Custom Fortify actions configured
     }
 
     /**
